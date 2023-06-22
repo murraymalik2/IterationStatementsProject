83fd08d4cd881eb755de57d1e156486b5798adc9
murra <murraymalik2@gmail.com>
2023-06-22 17:29:01 -0400

    luke warm

---
+++ LukeWarmSection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LukeWarmSection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {
+        for (int i = 1000; i >= -1000; i--) 
+            { 
+            Console.WriteLine(i);
+            }
+        for (int i = 3; i <= 999; i+=3) 
+            { 
+            Console.WriteLine(i);
+            }
+            if (num%2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+            
+
+
+        }
+    }
+}
